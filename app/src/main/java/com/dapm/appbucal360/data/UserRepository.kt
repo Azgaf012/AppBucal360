5b25e3cb25d304e68e27515613588f2ff0919d3a
Andree Zuniga <zuniga.andree@gmail.com>
2023-05-24 11:59:37 -0500

    registrar usuario

--- app/src/main/java/com/dapm/appbucal360/data/UserRepository.kt
+++ app/src/main/java/com/dapm/appbucal360/data/UserRepository.kt
@@ -1,17 +1,38 @@
 package com.dapm.appbucal360.data
 
 import com.dapm.appbucal360.model.user.LoggedInUser
+import com.dapm.appbucal360.model.user.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
+import java.util.Date
 
 class UserRepository{
 
     private val auth = FirebaseAuth.getInstance()
+    val db = Firebase.firestore
 
     suspend fun login(email: String, password: String): Result<LoggedInUser> {
         return runCatching {
             val result = auth.signInWithEmailAndPassword(email, password).await()
             val user = result.user
+            LoggedInUser(user?.uid ?: "", user?.email ?: "")
+        }
+    }
+
+    suspend fun register(email: String, password: String, firstName: String, lastName: String, phoneNumber: String, birthDate: Date): Result<LoggedInUser> {
+        return runCatching {
+            // Register the user in Firebase Auth.
+            val result = auth.createUserWithEmailAndPassword(email, password).await()
+            val user = result.user
+
+            // If the user was successfully registered, save their additional information in Firestore.
+            if (user != null) {
+                val firestoreUser = User(user.uid, firstName, lastName, phoneNumber, email, birthDate)
+                db.collection("paciente").document(user.uid).set(firestoreUser).await()
+            }
+
             LoggedInUser(user?.uid ?: "", user?.email ?: "")
         }
     }
--- app/src/main/java/com/dapm/appbucal360/presentation/register/RegisterViewModel.kt
+++ app/src/main/java/com/dapm/appbucal360/presentation/register/RegisterViewModel.kt
@@ -1,7 +1,30 @@
 package com.dapm.appbucal360.presentation.register
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dapm.appbucal360.data.UserRepository
+import com.dapm.appbucal360.model.user.RegistrationState
+import kotlinx.coroutines.launch
+import java.util.Date
 
 class RegisterViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val userRepository = UserRepository()
+
+    private val _registrationState = MutableLiveData<RegistrationState>()
+    val registrationState: LiveData<RegistrationState> = _registrationState
+
+    fun register(email: String, password: String, firstName: String, lastName: String, phoneNumber: String, birthDate: Date) {
+        viewModelScope.launch {
+            val result = userRepository.register(email, password, firstName, lastName, phoneNumber, birthDate)
+            if (result.isSuccess) {
+                _registrationState.value = RegistrationState.Success(result.getOrNull()!!)
+            } else {
+                _registrationState.value = RegistrationState.Error(result.exceptionOrNull())
+            }
+        }
+    }
+
 }
