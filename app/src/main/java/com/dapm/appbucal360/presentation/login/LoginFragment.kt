fd1fdf5affe1fdd9d778ddcf85fbdc54c691ffb9
Andree Zuniga <zuniga.andree@gmail.com>
2023-05-23 20:19:32 -0500

    modal olvide contrasena

--- app/src/main/java/com/dapm/appbucal360/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/dapm/appbucal360/presentation/login/LoginFragment.kt
@@ -1,7 +1,9 @@
 package com.dapm.appbucal360.presentation.login
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -32,6 +34,33 @@ class LoginFragment : Fragment() {
         val emailEditText = view.findViewById<EditText>(R.id.input_email)
         val passwordEditText = view.findViewById<EditText>(R.id.input_password)
         val loginButton = view.findViewById<Button>(R.id.btn_login)
+
+        val rxtModal = view.findViewById<TextView>(R.id.rxt_modal)
+        val modalOlvideContraseña = view.findViewById<LinearLayout>(R.id.modal_olvidéContraseña)
+
+        // Define un OnClickListener para rxtModal
+        rxtModal.setOnClickListener {
+            // Cuando rxtModal se haga clic, haz modalOlvideContraseña visible
+            modalOlvideContraseña.visibility = View.VISIBLE
+        }
+
+        modalOlvideContraseña.setOnTouchListener { _, _ ->
+            // Consume el evento de toque para evitar que se propague al layout padre
+            true
+        }
+
+        view.setOnTouchListener { _, event ->
+            // Si el modal está visible y se toca fuera de él, ocultarlo
+            if (modalOlvideContraseña.visibility == View.VISIBLE && event.action == MotionEvent.ACTION_DOWN) {
+                val rect = Rect()
+                modalOlvideContraseña.getGlobalVisibleRect(rect)
+                if (!rect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    modalOlvideContraseña.visibility = View.GONE
+                }
+            }
+            // No consumir el evento de toque para que otros elementos puedan manejarlo también
+            false
+        }
 
         /**[IMPLEMENTAR] Modificar el Gone del modal para que sea visible al hacer clic en olvidé mi contraseña**/
 
