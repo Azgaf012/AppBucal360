ad9dda7bef77f6ea9e31239b752326a48d111ac6
Jorge Mantilla <cokemanti@hotmail.com>
2023-06-01 07:41:47 -0500

    Eliminacion de cita Firebase(actualizar listado no funciona)

--- app/src/main/java/com/dapm/appbucal360/presentation/menu/citas_list/CitasFragment.kt
+++ app/src/main/java/com/dapm/appbucal360/presentation/menu/citas_list/CitasFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.dapm.appbucal360.R
@@ -21,6 +22,8 @@ import kotlin.collections.ArrayList
 class CitasFragment : Fragment() {
     private var listView: ListView? = null
     private var citas: ArrayList<String>? = null
+    private var citasIds: ArrayList<String>? = null
+    private var adapter: CitasAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +34,14 @@ class CitasFragment : Fragment() {
 
         listView = rootView.findViewById(R.id.listview_citas)
         citas = ArrayList()
+        citasIds = ArrayList()
+
+        adapter = CitasAdapter(
+            requireContext(),
+            citas!!,
+            citasIds!! // Pasar la lista de IDs al adaptador
+        )
+        listView?.adapter = adapter
 
         val db = FirebaseFirestore.getInstance()
         val citasCollection = db.collection("citas")
@@ -38,25 +49,27 @@ class CitasFragment : Fragment() {
         citasCollection.get()
             .addOnSuccessListener { querySnapshot: QuerySnapshot? ->
                 querySnapshot?.let {
+                    citas?.clear()
+                    citasIds?.clear()
+
                     for (document in querySnapshot) {
+                        val id = document.id // Obtener el ID del documento
                         val fechaCita = document.getTimestamp("fecha_cita")
                         val nombreDoctorCita = document.getString("nombre_doctor_cita")
                         fechaCita?.let {
                             val fechaTexto = formatDate(it)
                             val citaTexto = "$fechaTexto\nDoctor: $nombreDoctorCita"
-                            citas!!.add(citaTexto)
+                            citas?.add(citaTexto)
+                            citasIds?.add(id) // Agregar el ID a la lista de IDs
                         }
                     }
 
-                    val adapter = CitasAdapter(
-                        requireContext(),
-                        citas!!
-                    )
-                    listView?.adapter = adapter
+                    adapter?.notifyDataSetChanged()
                 }
             }
             .addOnFailureListener { exception ->
                 // Manejar el caso de error aquí
+                // Por ejemplo, mostrar un mensaje de error al usuario
             }
 
         return rootView
@@ -81,8 +94,14 @@ class CitasFragment : Fragment() {
         return formattedDate
     }
 
-    private class CitasAdapter(context: Context, citas: ArrayList<String>) :
+    private inner class CitasAdapter(
+        context: Context,
+        citas: ArrayList<String>,
+        ids: ArrayList<String>
+    ) :
         ArrayAdapter<String>(context, 0, citas) {
+
+        private val citasIds: ArrayList<String> = ids
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
             var view = convertView
@@ -93,8 +112,7 @@ class CitasFragment : Fragment() {
             }
 
             val textView = view?.findViewById<TextView>(R.id.text1)
-            val button = view?.findViewById<ImageButton>(R.id.button1)
-
+            val button = view?.findViewById<ImageButton>(R.id.eliminar_cita)
 
             textView?.text = cita
             textView?.setTextColor(Color.BLACK)
@@ -102,14 +120,45 @@ class CitasFragment : Fragment() {
             textView?.textSize = 18f
             textView?.setPadding(50, 10, 0, 10)
 
-            button?.setOnClickListener {
-                // Acción que se realizará al hacer clic en el botón
-                // Puedes implementar aquí la redirección o cualquier otra lógica que desees
+            // Verificar si la posición es válida y no está fuera de límites
+            if (position >= 0 && position < count && position < citasIds.size) {
+                val citaId = citasIds[position] // Obtener el ID correspondiente a la posición
+
+                button?.setOnClickListener {
+                    showConfirmationDialog(citaId, position)
+                }
             }
 
-
-
             return view!!
+        }
+
+        private fun showConfirmationDialog(citaId: String, position: Int) {
+            val alertDialogBuilder = AlertDialog.Builder(context)
+            alertDialogBuilder.setTitle("Eliminación de cita")
+            alertDialogBuilder.setMessage("¿Estás seguro de que quieres eliminar esta cita?")
+            alertDialogBuilder.setPositiveButton("Sí") { _, _ ->
+                eliminarCita(citaId, position)
+            }
+            alertDialogBuilder.setNegativeButton("No", null)
+            alertDialogBuilder.show()
+        }
+
+        private fun eliminarCita(citaId: String, position: Int) {
+            val db = FirebaseFirestore.getInstance()
+            val citasCollection = db.collection("citas")
+
+            citasCollection.document(citaId)
+                .delete()
+                .addOnSuccessListener {
+                    // Eliminación exitosa, realizar las acciones necesarias (redirección, actualización, etc.)
+                    citasIds.removeAt(position)
+                    notifyDataSetChanged()       //NO FUNCIONA EL REFRESCAR LA LISTA
+                    requireActivity().recreate()   //REFRESCA LA VISTA COMPLETO, SOLUCION TEMPORAL
+                }
+                .addOnFailureListener { exception ->
+                    // Manejar el caso de error aquí
+                    // Por ejemplo, mostrar un mensaje de error al usuario
+                }
         }
     }
 
@@ -119,7 +168,7 @@ class CitasFragment : Fragment() {
         val back = view.findViewById<ImageButton>(R.id.imageButton)
 
         back.setOnClickListener {
-                Navigation.findNavController(view).navigate(R.id.action_citasFragment_to_menuFragment)
+            Navigation.findNavController(view).navigate(R.id.action_citasFragment_to_menuFragment)
         }
     }
 }
