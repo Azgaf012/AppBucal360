3366c1d0557bd1c7ff4fac64e7f5f5bddf5bf9c8
Jampier <jampier081@gmail.com>
2023-07-12 17:31:26 -0500

    onBoarding terminado

--- app/src/main/java/com/dapm/appbucal360/presentation/menu/MenuFragment.kt
+++ app/src/main/java/com/dapm/appbucal360/presentation/menu/MenuFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -36,17 +37,59 @@ class MenuFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val archivoLogin = File(requireContext().filesDir, "login_1")
+        //Usar la línea de bajo para borrar archivo
+        //requireContext().deleteFile("login_1")
+
+        val onboardingLayout = view.findViewById<View>(R.id.onboarding_background)
+        val screen2 = view.findViewById<View>(R.id.onboarding_2)
+        val screen3 = view.findViewById<View>(R.id.onboarding_3)
+        val screen4 = view.findViewById<View>(R.id.onboarding_4)
+        val btn_screen1_to_screen2 = view.findViewById<Button>(R.id.btn_nxt_screen_1)
+        val btn_screen2_to_screen1 = view.findViewById<Button>(R.id.btn_prv_screen_2)
+        val btn_screen2_to_screen3 = view.findViewById<Button>(R.id.btn_nxt_screen_2)
+        val btn_screen3_to_screen2 = view.findViewById<Button>(R.id.btn_prv_screen_3)
+        val btn_screen3_to_screen4 = view.findViewById<Button>(R.id.btn_nxt_screen_3)
+        val btn_screen4_to_screen3 = view.findViewById<Button>(R.id.btn_prv_screen_4)
+        val btn_screen4_to_menu = view.findViewById<Button>(R.id.btn_finish_screen_4)
+
+         // /**
 
         if (!archivoLogin.exists()) {
             archivoLogin.createNewFile()
-            val onboardingLayout = view.findViewById<View>(R.id.onboarding_background)
             onboardingLayout.visibility = View.VISIBLE
-            Log.d("No Existía","Existía")
+            Log.d("Login_1","Se creó el archivo")
+            btn_screen1_to_screen2.setOnClickListener{
+                onboardingLayout.visibility = View.GONE
+                screen2.visibility = View.VISIBLE
+            }
+            btn_screen2_to_screen1.setOnClickListener{
+                screen2.visibility = View.GONE
+                onboardingLayout.visibility = View.VISIBLE
+            }
+            btn_screen2_to_screen3.setOnClickListener{
+                screen2.visibility = View.GONE
+                screen3.visibility = View.VISIBLE
+            }
+            btn_screen3_to_screen4.setOnClickListener{
+                screen3.visibility = View.GONE
+                screen4.visibility = View.VISIBLE
+            }
+            btn_screen3_to_screen2.setOnClickListener{
+                screen3.visibility = View.GONE
+                screen2.visibility = View.VISIBLE
+            }
+            btn_screen4_to_screen3.setOnClickListener{
+                screen4.visibility = View.GONE
+                screen3.visibility = View.VISIBLE
+            }
+            btn_screen4_to_menu.setOnClickListener{
+                screen4.visibility = View.GONE
+            }
         } else {
-            val onboardingLayout = view.findViewById<View>(R.id.onboarding_background)
             onboardingLayout.visibility = View.GONE
-            Log.d("Sí Existe","Existe")
+            Log.d("Login_1","El archivo ya existía")
         }
+        // **/
 
         val nombreUsuario: TextView = view.findViewById(R.id.nombreUsuario)
         nombreUsuario.text = "Hola, ${userViewModel.loggedInUser.value?.firstName}"
